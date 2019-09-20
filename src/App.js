6732f8392b044ed8e7baa0b4ea16feceee3d6226
Jorge Barcos <jorgebarcos88@gmail.com>
2019-09-20 19:38:31 -0300

    Mostrar el Error de la validación

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
+import Error from './components/Error';
 
 function App() {
 	// State Principal
@@ -8,18 +9,30 @@ function App() {
 	// ciudad = state, guardarCiudad = this.setState()
 	const [ ciudad, guardarCiudad ] = useState('');
 	const [ pais, guardarPais ] = useState('');
+	const [ error, guardarError ] = useState(false);
 
 	const datosConsulta = (datos) => {
 		// Validar que ambos campos estén
 		if (datos.ciudad === '' || datos.pais === '') {
-			// un error
+			guardarError(true);
 			return;
 		}
 
 		// Ciudad y pais existen, agregarlos al state
 		guardarCiudad(datos.ciudad);
 		guardarPais(datos.pais);
+		guardarError(false);
 	};
+
+	// Cargar un component Condicionalmente
+	let componente;
+	if (error) {
+		// Hay un error, mostrarlo
+		componente = <Error mensaje="Ambos campos son obligatorios" />;
+	} else {
+		// Mostrar el Clima
+		componente = null;
+	}
 	return (
 		<div className="App">
 			<Header titulo="Clima React App" />
@@ -30,6 +43,7 @@ function App() {
 						<div className="col s12 m6">
 							<Formulario datosConsulta={datosConsulta} />
 						</div>
+						<div className="col s12 m6">{componente}</div>
 					</div>
 				</div>
 			</div>
